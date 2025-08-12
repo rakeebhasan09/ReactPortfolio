2237c17eee6a9ced659ac9a061119acf0242d7b1
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-08-12 16:15:44 +0600

    Some experiments have been conducted on changing the position of the hero image.

--- src/Pages/Home/HeroBanner/HeroBanner.jsx
+++ src/Pages/Home/HeroBanner/HeroBanner.jsx
@@ -8,7 +8,7 @@ const HeroBanner = () => {
 			<div className="bg-[url('https://i.ibb.co.com/WpHTVz9x/banner-background-one.jpg')] bg-no-repeat bg-cover bg-center pt-24 md:pt-[110px] lg:pt-[120px]">
 				<div className="container">
 					<div className="flex flex-col gap-y-8 lg:flex-row items-center">
-						<div className="lg:w-1/2">
+						<div className="w-full lg:w-1/2">
 							<div className="content">
 								<span className="text-white text-[28px] font-rajdhani font-bold uppercase">
 									hello
@@ -16,7 +16,6 @@ const HeroBanner = () => {
 								<h1 className="text-white text-[40px] sm:text-[50px] md:text-[60px] lg:text-[54px] xl:text-[65px] xxl:text-[68px] font-rajdhani font-bold">
 									I'm Rakeeb Hasan a
 								</h1>
-
 								<div className="text-[40px] sm:text-[50px] md:text-[60px] lg:text-[45px] xl:text-[55px] xxl:text-[68px]">
 									<TypeWriterEffect
 										textStyle={{
@@ -53,13 +52,15 @@ const HeroBanner = () => {
 								</Link>
 							</div>
 						</div>
-						<div className="lg:w-1/2">
+						<div className="w-full lg:w-1/2">
 							<div className="my-image relative">
-								<img
-									className="block ml-auto z-10 relative"
-									src="https://i.ibb.co.com/9mzCC32q/banner-user-image-one.png"
-									alt="MD RAKEEB HASAN"
-								/>
+								<div>
+									<img
+										className="block mx-auto xl:ml-auto z-10 relative"
+										src="https://i.ibb.co.com/9mzCC32q/banner-user-image-one.png"
+										alt="MD RAKEEB HASAN"
+									/>
+								</div>
 								{/* Bottom Text */}
 								<div className="absolute bottom-0 sm:bottom-2 md:bottom-3 left-2/4 -translate-x-2/4 xl:left-3/4 xl:-translate-x-3/4 xxl:left-auto xxl:-translate-x-0 xxl:-right-7 z-10">
 									<h2 className="text-[45px] sm:text-[70px] whitespace-nowrap md:text-[80px] lg:text-[70px] xl:text-[80px] xxl:text-[90px] font-rajdhani font-bold custom-stroke">
