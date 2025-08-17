1e10d1aa4d7d7fd9222d3615344e9d1fff1eebe5
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-08-17 13:53:46 +0600

    Animated border added as a mouse hover effect.

--- src/Pages/Home/CounterArea/CounterCard.jsx
+++ src/Pages/Home/CounterArea/CounterCard.jsx
@@ -1,6 +1,23 @@
+import { useRef } from "react";
 const CounterCard = ({ ammount, subTitle }) => {
+	const cardRef = useRef(null);
+
+	const handleMouseMove = (e) => {
+		const rect = cardRef.current.getBoundingClientRect();
+		const mouseX = e.clientX - rect.left - rect.width / 2;
+		const mouseY = e.clientY - rect.top - rect.height / 2;
+
+		let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+		angle = (angle + 360) % 360;
+
+		cardRef.current.style.setProperty("--start", angle + 60);
+	};
 	return (
-		<div className="w-full md:w-[calc(50%-15px)] bg-[#141414] rounded-[20px] py-[30px] px-[7px]">
+		<div
+			ref={cardRef}
+			onMouseMove={handleMouseMove}
+			className="w-full md:w-[calc(50%-15px)] bg-[#141414] rounded-[20px] py-[30px] px-[7px] card"
+		>
 			<h2 className="text-white text-[48px] font-rajdhani font-black text-center">
 				{ammount}
 			</h2>
